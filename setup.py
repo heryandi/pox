770acc64de7583f92ee359ba96a03c96c601ed26
Heryandi <heryandi@gmail.com>
2013-07-02 10:25:26 +0800

    Quick hack using setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pox',
+    version='0.0.0',
+    author='Murphy McCauley',
+    author_email='murphy.mccauley@gmail.com',
+    packages=find_packages(exclude='test'),
+    scripts=['pox.sh'],
+    url='http://www.noxrepo.org/pox/about-pox/',
+    license='IDUNNO',
+    description='pox controller',
+    long_description=open('README').read(),
+    install_requires=[
+        'distribute'
+    ],
+)
